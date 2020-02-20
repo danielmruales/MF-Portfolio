137ac19c819765d9b7b994d89b02ad15201293d5
Daniel Ruales <danielmruales@gmail.com>
2020-02-20 09:56:40 -0700

    fixed dom rendering bug

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,8 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 
 
-ReactDOM.render( 
-        <BrowserRouter> <App/> </BrowserRouter>)
+ReactDOM.render(
+                <BrowserRouter>
+                        <App/>
+                </BrowserRouter>, document.getElementById('root')
+                )
