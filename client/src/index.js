d7a14bbec57c3340a6f154776fd93e459b8118d3
Daniel Ruales <danielmruales@gmail.com>
2020-02-19 22:47:43 -0700

    test

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
-import GlobalData from './GlobalData'
 
 
-ReactDOM.render( <GlobalData>
-                    <BrowserRouter>
-                        <App/>
-                    </BrowserRouter>
-                </GlobalData>, document.getElementById('root'));
-
+ReactDOM.render( 
+        <BrowserRouter> <App/> </BrowserRouter>)
